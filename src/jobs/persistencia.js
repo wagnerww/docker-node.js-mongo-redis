6184238567ada94d10b1fa65779f6b3c45829938
wagner <wagnerricardonet@gmail.com>
2019-02-08 07:37:34 -0200

    Testando a execução de jobs

--- src/jobs/persistencia.js
+++ src/jobs/persistencia.js
@@ -1,7 +1,10 @@
-const redis = require('../conf/redis');
-const emailModel = require('../Models/emailModel');
+//const redis = require('../conf/redis');
+//const emailModel = require('../Models/emailModel');
+const schedule  = require('node-schedule');
 
-persistir = async () => {
+let exec = 0;
+const segundosExec = 2;
+/*persistir = async () => {
 
     await redis.smembers("sendEmail", function (err, values) {
         console.log('valores', values)
@@ -15,6 +18,10 @@ persistir = async () => {
                 redis.SREM("sendEmail", value)
             }
     });
-}
+}*/
 
-setInterval(persistir, 5000);
+const job = schedule.scheduleJob('0-59/5 * * * * *', async () => {
+   exec += 1;
+   await console.log(`execução número ${exec}, tempo:${segundosExec * exec}`);
+});
+//setInterval(persistir, 5000);
