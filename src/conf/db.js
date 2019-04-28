da6efc8cb04d26b5f21264c91b9e36a72e220181
wagner <wagnerricardonet@gmail.com>
2019-04-28 20:01:59 -0300

    ajuste no compose

--- src/conf/db.js
+++ src/conf/db.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 const host = "mongo";
 const port = "27017";
 
-mongoose.connect(`mongodb://${host}:${port}/email`);
-
-mongoose.connection.on('connected', function(){
+mongoose.connect(`mongodb://${host}:${port}/email`).then(connect => {
     console.log("==> conexão com o MONGO OK!");
-});
-
-mongoose.connection.on('error', function(){
+}).catch(err => {
     console.log("==> falha na conexão com o MongoDB!");
 });
+/*
+mongoose.connection.on('connected', function () {
+
+});
+
+mongoose.connection.on('error', function () {
+
+});*/
 
 module.exports = mongoose;
--- src/jobs/persistencia.js
+++ src/jobs/persistencia.js
@@ -1,27 +1,23 @@
-//const redis = require('../conf/redis');
-//const emailModel = require('../Models/emailModel');
-const schedule  = require('node-schedule');
+const redis = require("../conf/redis");
+const emailModel = require("../Models/emailModel");
+const schedule = require("node-schedule");
 
 let exec = 0;
-const segundosExec = 2;
 persistir = async () => {
+  await redis.smembers("sendEmail", function(err, values) {
+    if (!err)
+      for (i in values) {
+        let value = values[i];
+        let email = JSON.parse(value);
+        emailModel.create(email);
 
-    await redis.smembers("sendEmail", function (err, values) {
-        console.log('valores', values)
-        if (!err)
-            for (i in values) {
-                let value = values[i];
-                let email = JSON.parse(value);
-                console.log('óbjeto',email)
-                emailModel.create(email);
+        redis.SREM("sendEmail", value);
+      }
+  });
+};
 
-                redis.SREM("sendEmail", value)
-            }
-    });
-}
-
-const job = schedule.scheduleJob('0-59/5 * * * * *', async (date) => {
-   exec += 1;
-   await console.log(`execução número:${exec}, tempo decorrido:${segundosExec * exec}, hora:${date}`);
+const job = schedule.scheduleJob("0-59/5 * * * * *", async date => {
+  exec += 1;
+  await persistir();
+  console.log(`execução número:${exec}, hora:${date}`);
 });
-//setInterval(persistir, 5000);
